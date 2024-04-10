66e5357f73fe02f8499a1da8aeb47b93637e59dc
Eisler2712 <eislervalles@gmail.com>
2024-04-09 20:39:32 -0400

    Descomment Methods

--- ConsoleHttpClient/Program.cs
+++ ConsoleHttpClient/Program.cs
@@ -19,22 +19,22 @@ class Program
         Console.WriteLine("Headers: " + string.Join(", ", headers.Select(x => $"{x.Key}: {x.Value}")));
         Console.WriteLine("Body: " + body);
         
-        // // Test PUT request
-        // (headers, body) = client.SendRequest("PUT", "/put", body: "This is a PUT request");
-        // Console.WriteLine("PUT /put");
-        // Console.WriteLine("Headers: " + string.Join(", ", headers.Select(x => $"{x.Key}: {x.Value}")));
-        // Console.WriteLine("Body: " + body);
-        //
-        // // Test DELETE request
-        // (headers, body) = client.SendRequest("DELETE", "/delete");
-        // Console.WriteLine("DELETE /delete");
-        // Console.WriteLine("Headers: " + string.Join(", ", headers.Select(x => $"{x.Key}: {x.Value}")));
-        // Console.WriteLine("Body: " + body);
-        //
-        // // Test PATCH request
-        // (headers, body) = client.SendRequest("PATCH", "/patch", body: "This is a PATCH request");
-        // Console.WriteLine("PATCH /patch");
-        // Console.WriteLine("Headers: " + string.Join(", ", headers.Select(x => $"{x.Key}: {x.Value}")));
-        // Console.WriteLine("Body: " + body);
+        // Test PUT request
+        (headers, body) = client.SendRequest("PUT", "/put", body: "This is a PUT request");
+        Console.WriteLine("PUT /put");
+        Console.WriteLine("Headers: " + string.Join(", ", headers.Select(x => $"{x.Key}: {x.Value}")));
+        Console.WriteLine("Body: " + body);
+        
+        // Test DELETE request
+        (headers, body) = client.SendRequest("DELETE", "/delete");
+        Console.WriteLine("DELETE /delete");
+        Console.WriteLine("Headers: " + string.Join(", ", headers.Select(x => $"{x.Key}: {x.Value}")));
+        Console.WriteLine("Body: " + body);
+        
+        // Test PATCH request
+        (headers, body) = client.SendRequest("PATCH", "/patch", body: "This is a PATCH request");
+        Console.WriteLine("PATCH /patch");
+        Console.WriteLine("Headers: " + string.Join(", ", headers.Select(x => $"{x.Key}: {x.Value}")));
+        Console.WriteLine("Body: " + body);
     }
 }
